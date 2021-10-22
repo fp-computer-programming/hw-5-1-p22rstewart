70b96f46cf22968ad435d6d8fac3e6dcc9540795
p22rstewart <p22rstewart@fairfieldprep.org>
2021-10-22 09:58:41 -0400

    hw

---
+++ hw5-1.py
@@ -0,0 +1,18 @@
+#Author: RTS 10/22/21
+import random
+random.seed(16)
+
+#Question 1
+print(random.randint(31, 49))
+
+#Question 2
+print(random.randrange(3, 75, 3))
+
+#Question 3
+print(random.randrange(20, 31, 3))
+
+#Question 4
+print(random.randint(1, 8))
+
+#Question 5
+print(random.randint(1, 20))
