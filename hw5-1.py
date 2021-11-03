277fc7655e058ed143ed4727c7b7e8bcf8403f04
Ryan Stewart <P22rstewart@fairfieldprep.org>
2021-11-02 23:42:21 -0400

    5-1

--- hw5-1.py
+++ hw5-1.py
@@ -16,3 +16,21 @@ print(random.randint(1, 8))
 
 # Question 5
 print(random.randint(1, 20))
+
+# Question 6
+print(random.choice(['cat', 'dog', 'sheep', 'cow', 'chicken', 'pig']))
+
+# Question 7 
+print(random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4))
+
+# Question 8
+print(random.sample([1, 2, 3, 4, 5], 5))
+
+# Question 9
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
+random.shuffle(cards)
+print(cards)
+
+# Question 10
+random.seed(1942)
+print(random.randint(1, 1000))
